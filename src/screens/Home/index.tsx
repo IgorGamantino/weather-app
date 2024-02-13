42a91b8691ec935fa56fb6b4f4aa5020b68b8a43
Igor Amantino <igor.amantinodev@hotmail.com>
2024-02-13 19:59:47 -0300

    feat/finish-home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,148 @@
 import * as S from "./styles"
 
 import SunLogo from '../../assets/Sun-icon.svg'
-export function Home () {
-  return (
-    <S.Container>
-      <S.Linear 
-       colors={['#7abcff', '#59abf8', '#4096ee']}
-      />
-      <S.Title>
-        Container Home
-      </S.Title>
+import DayCloudLogo from '../../assets/DayClouds.svg'
+import CalendarLogo from '../../assets/calendar.svg'
 
-      <SunLogo />
-    </S.Container>
+import { BackdropBlur, Canvas, Fill } from '@shopify/react-native-skia'
+import { ScrollView } from "react-native"
+export function Home() {
+  return (
+    <ScrollView>
+      <S.Linear
+        colors={['#7abcff', '#59abf8', '#4096ee']}
+      />
+      <S.Container>
+        <SunLogo width={200} height={200} />
+        <S.Title>
+          28º
+        </S.Title>
+
+        <S.PrecipitationsText>Precipitations</S.PrecipitationsText>
+        <S.PrecipitationsText>Max: 34 Min: 28</S.PrecipitationsText>
+
+
+        <S.WrapperCondition>
+          <S.PrecipitationsText>18%</S.PrecipitationsText>
+          <S.PrecipitationsText>18%</S.PrecipitationsText>
+          <S.PrecipitationsText>18%</S.PrecipitationsText>
+        </S.WrapperCondition>
+
+        <S.WrapperTodayAllCondition>
+          <S.WrapperDayNow>
+            <S.TextDayNow>Today</S.TextDayNow>
+            <S.TextDay>Mar,10</S.TextDay>
+          </S.WrapperDayNow>
+
+          <S.ListDayWrapper>
+            <S.WrapperDateTime>
+              <S.TextTemperature>
+                31C
+              </S.TextTemperature>
+              <DayCloudLogo width={40} height={40} />
+
+              <S.TextHour>15:00</S.TextHour>
+            </S.WrapperDateTime>
+
+            <S.WrapperDateTime>
+              <S.TextTemperature>
+                31C
+              </S.TextTemperature>
+              <DayCloudLogo width={40} height={40} />
+
+              <S.TextHour>15:00</S.TextHour>
+            </S.WrapperDateTime>
+
+            <S.WrapperDateTime>
+              <S.TextTemperature>
+                31C
+              </S.TextTemperature>
+              <DayCloudLogo width={40} height={40} />
+
+              <S.TextHour>15:00</S.TextHour>
+            </S.WrapperDateTime>
+
+            <S.WrapperDateTime>
+              <S.TextTemperature>
+                31C
+              </S.TextTemperature>
+              <DayCloudLogo width={40} height={40} />
+
+              <S.TextHour>15:00</S.TextHour>
+            </S.WrapperDateTime>
+
+            <S.WrapperDateTime>
+              <S.TextTemperature>
+                31C
+              </S.TextTemperature>
+              <DayCloudLogo width={40} height={40} />
+
+              <S.TextHour>15:00</S.TextHour>
+            </S.WrapperDateTime>
+
+
+
+          </S.ListDayWrapper>
+        </S.WrapperTodayAllCondition>
+
+
+        <S.WrapperNextForecast>
+          <S.WrapperDayNow>
+            <S.TextDayNow>Next Forecast</S.TextDayNow>
+            <CalendarLogo width={24} height={24} />
+          </S.WrapperDayNow>
+
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+          <S.WrapperListDay>
+
+            <S.TextDayList>Monday</S.TextDayList>
+            <DayCloudLogo width={40} height={40} />
+
+            <S.TextTemperatureList>13</S.TextTemperatureList>
+          </S.WrapperListDay>
+        </S.WrapperNextForecast>
+      </S.Container>
+    </ScrollView>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,8 +3,12 @@ import {LinearGradient} from 'expo-linear-gradient';
 
 export const Container = styled.View`
   flex: 1;
+  padding-top:80px;
+  padding-bottom:30px;
   justify-content: center;
   align-items: center;
+  paddingHorizontal: 30px;
+  text-align: center;
 `;
 
 export const Linear = styled(LinearGradient)`
@@ -13,9 +17,124 @@ export const Linear = styled(LinearGradient)`
   left: 0;
   right: 0;
   top: 0;
-  height: 100%
+  height: 100%;
+`;
+
+export const Title = styled.Text`
+  font-size: 64px;
+  font-weight: bold;
+  color: #fff;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const PrecipitationsText = styled.Text`
+font-size: 18px;
+color: #fff;
+`;
+
+export const WrapperCondition = styled.View`
+ height: 47px;
+ margin-top: 20px;
+ border-radius: 20px;
+ width: 100%;
+ background-color: rgba(0,128,255,0.5);
+ flex-direction: row;
+ align-items: center;
+ paddingHorizontal: 15px;
+ justify-content: space-between;
+`;
+
+export const TextCondition = styled.Text``;
+
+
+export const WrapperTodayAllCondition = styled.View`
+ height: 217px;
+ margin-top: 20px;
+ border-radius: 20px;
+ width: 100%;
+ background-color: rgba(0,128,255,0.5);
+ 
+ padding: 15px;
+
+`;
+
+export const WrapperDayNow = styled.View`
+flex-direction: row;
+justify-content: space-between;
+align-items:center;
+margin-bottom: 10px;
+`;
+
+export const ListDayWrapper = styled.ScrollView.attrs({
+  horizontal:true,
+  showsHorizontalScrollIndicator: false
+})`
+
+`
+
+export const TextDayNow = styled.Text`
+font-size: 20px;
+font-weight: bold;
+color: #fff;
 
 
 `;
 
-export const Title = styled.Text``;
+export const TextDay = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`;
+
+
+export const WrapperDateTime = styled.View`
+  padding-top: 10px;
+  padding-bottom: 10px;
+  height: 155px;
+  justify-content:space-between;
+  margin-right: 34px;
+`;
+
+export const TextTemperature = styled.Text`
+  color: #fff;
+  font-size: 18px;
+`;
+
+export const TextHour = styled.Text`
+  color: #fff;
+  font-size: 18px;
+`;
+
+
+export const WrapperNextForecast = styled.View`
+ margin-top: 20px;
+ border-radius: 20px;
+ width: 100%;
+ background-color: rgba(0,128,255,0.5);
+ padding: 15px;
+`;
+
+export const ListDays = styled.ScrollView``;
+
+
+export const WrapperListDay = styled.View`
+ flex-direction: row;
+ justify-content: space-between;
+ align-items:center;
+ margin-bottom: 20px;
+`;
+
+export const TextDayList = styled.Text`
+ color: #fff;
+ font-size: 18px;
+ font-weight: bold;
+
+`;
+
+
+export const TextTemperatureList = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500
+
+`;
